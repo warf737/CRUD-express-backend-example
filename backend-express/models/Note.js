c61727e1fa72c76691083bae7bbc6d6f7ee36b4f
dvasilchuk <dvasilchuk@phoenixit.ru>
2020-02-03 00:16:14 +0300

    fixes api

--- backend-express/models/Note.js
+++ backend-express/models/Note.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 
 const NoteSchema = new Schema({
+  cuid: { type: 'String', required: true },
   title: {type: 'String', required: true },
   content: {type: 'String', required: true},
-  dateAdded: {type: 'Date', default: Date.now, required:true }
-});
+},{ timestamps: true });
 
 let Note = mongoose.model('Note', NoteSchema);
 
